8761a0dcab99800ec5d6a1791b76ef5a2b247f8c
Ahmad Fadly Muktafi <bedasendiri28@gmail.com>
2026-06-25 15:17:39 +0700

    model

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
     'version': '19.0.1.0.0',
     'summary': 'Manajemen Servis Alat Kesehatan',
     'author': 'Fadly Muktafi - Junior Odoo Programmer',
-    'category': 'Healthcare',
+    'category': 'Inventory',
     'depends': ['base', 'product', 'contacts'],
     'data': [
         'security/ir.model.access.csv',
---
+++ models/servis.py
@@ -0,0 +1,23 @@
+from odoo import models, fields
+
+class ServisAlatKesehatan(models.Model):
+    _name = 'servis.alat.kesehatan'
+    _description = 'Manajemen Servis Alat Kesehatan'
+    _order = 'servis_masuk desc'
+    _rec_name = 'name'
+
+    name = fields.Char(string='Kode Servis', required=True)
+    customer_id = fields.Many2one('res.partner', string='Customer', required=True)
+    product_id = fields.Many2one('product.product', string='Alat Kesehatan', required=True)
+    technician_id = fields.Many2one('res.users', string='Teknisi', required=True)
+    servis_masuk = fields.Datetime(string='Servis Masuk', required=True)
+    perkiraan_selesai = fields.Datetime(string='Perkiraan Servis Selesai')
+    servis_selesai = fields.Datetime(string='Servis Selesai')
+    complaint = fields.Text(string='Keluhan Customer')
+    result = fields.Text(string='Hasil Servis')
+    status = fields.Selection([
+        ('draft', 'Draft'),
+        ('progress', 'Servis'),
+        ('done', 'Selesai'),
+        ('cancel', 'Batal'),
+    ], string='Status', default='draft')
